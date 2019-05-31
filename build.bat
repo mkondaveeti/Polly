1e0fd95684db227942e20f804022bb0434c917d9
mkondaveeti <48488626+kondamx@users.noreply.github.com>
2019-05-31 14:37:11 -0500

    dummy

--- build.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-PUSHD %~dp0
-PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& './build.ps1'"
-
-IF %errorlevel% neq 0 PAUSE
-
--- src/Polly.Shared/ExceptionPredicate.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace Polly
-{
-    internal delegate Exception ExceptionPredicate(Exception ex); 
-}
